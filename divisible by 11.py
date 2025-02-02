9b1070c62ca5b9744f25da30bb768dd597269af6
bmS011 <144057395+bmS011@users.noreply.github.com>
2025-02-02 11:56:10 +0300

    Create divisible by 11.py

---
+++ divisible by 11.py
@@ -0,0 +1,8 @@
+#A program to if a number is divisble by 11
+
+number = int(input("Enter a number: "))
+
+if number % 11==0:
+     print("The number is divisible by 11.")
+else:
+    print("The number is not divisible by 11.")
