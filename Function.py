6f3550f8d796ffd6ef73f0f1b759be057a63d852
MwendaBrian01 <144057395+MwendaBrian01@users.noreply.github.com>
2025-02-15 13:46:39 +0300

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,12 @@
+import math
+dir(math)
+
+print(math.cos(90))
+print(math.floor(5.45678))
+#python-user defined
+def sum(x,y):
+    return x+y
+num1=int(input("Enter num1:"))
+num2=float(input("Enter num2:"))
+print(sum(num1,num2))
+print(sum(10,50))
